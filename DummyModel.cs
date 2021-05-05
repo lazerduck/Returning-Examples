49d36720519ac97c253b72380ef2e43f9730a006
Adam Bowes <skylinegames@hotmail.co.uk>
2021-05-05 21:05:56 +0100

    Enable nullable

--- DummyModel.cs
+++ DummyModel.cs
@@ -2,9 +2,9 @@ namespace Returning_Examples
 {
     public class DummyModel
     {
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
-        public string Prop1 { get; set; }
+        public string? Prop1 { get; set; }
 
         public int Prop2 { get; set; }
     }
